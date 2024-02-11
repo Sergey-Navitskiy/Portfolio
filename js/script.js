e41a9e323fb2f7493e9f105fa9299cfc8def36ac
Sergey-Navitskiy <126409337+Sergey-Navitskiy@users.noreply.github.com>
2024-02-11 21:06:02 +0300

    5th section

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
-const hamburger = document.querySelector('.hamburger'), menu = document.querySelector('.menu')
-closeElem = document.querySelector('.menu__close')
+const hamburger = document.querySelector(".hamburger"),
+  menu = document.querySelector(".menu");
+closeElem = document.querySelector(".menu__close");
 
-hamburger.addEventListener('click', function(){
-    menu.classList.add('active')
-})
+hamburger.addEventListener("click", function () {
+  menu.classList.add("active");
+});
 
-closeElem.addEventListener('click', function() {
-    menu.classList.remove('active')
-})
+closeElem.addEventListener("click", function () {
+  menu.classList.remove("active");
+});
+
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+      lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
